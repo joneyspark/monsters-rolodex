5e3bc46a1b7b3c04d6ff9c2e1d5cd46f125a31ba
Joney Spark <sparkjoney@gmail.com>
2021-02-15 11:22:43 +0600

    Monster Rolodex

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './card-list-styles.css'
+import { Card } from './Card.component'
+
+export  const CardList = props => {
+    
+    console.log(props.monster)
+    
+  
+    
+    return (
+        <div className="card-list">
+
+            {
+                props.monster
+                ? 
+                props.monster.map(monster => (
+                    <Card key={monster.id} monster={monster} />
+                ))
+                :
+                <h1>Loading...</h1>
+            }
+        </div>
+    )
+}
---
+++ src/components/card-list/card/Card.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const card = (props) => {
+    return (
+        <div>
+            <h1>{props.monster.name}</h1>
+        </div>
+    )
+}
